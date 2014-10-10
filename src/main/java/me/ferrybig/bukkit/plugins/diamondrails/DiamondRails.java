e6328abbe0179a898ae783b2067866ca7aaa3c49
Fernando van Loenhout <ferrytinymailbox@gmail.com>
2014-10-10 23:52:39 +0200

    To prevent minecarts from falling of tracks, they will now search for diamond blocks near their locations instead of directly below their location

--- src/main/java/me/ferrybig/bukkit/plugins/diamondrails/DiamondRails.java
+++ src/main/java/me/ferrybig/bukkit/plugins/diamondrails/DiamondRails.java
@@ -31,6 +31,20 @@ public class DiamondRails extends JavaPlugin implements Listener {
         }
     }
 
+    private static final BlockFace[] BLOCK_LOCATIONS;
+
+    static {
+        BLOCK_LOCATIONS = new BlockFace[9 * 3];
+        int i = 0;
+        for (int x = -1; x < 2; x++) {
+            for (int z = -1; z < 2; z++) {
+                for (int y = -1; y < 2; y++) {
+                    BLOCK_LOCATIONS[i++] = new BlockFace(x, y, z);
+                }
+            }
+        }
+    }
+
     @Override
     public void onEnable() {
         this.getServer().getPluginManager().registerEvents(this, this);
@@ -49,28 +63,27 @@ public class DiamondRails extends JavaPlugin implements Listener {
                 return;
             }
             loc.add(0, SEARCH_DEPTH, 0);
-            Block search = loc.getBlock();
-            // Duo to gravity changes, the minecart may hover down
-            Block belowSearch = search.getRelative(0, 1, 0);
-            if (search.getType() == TRACK_MATERIAL
-                    || belowSearch.getType() == TRACK_MATERIAL) {
-                loc = loc.add(minecart.getVelocity().multiply(4));
-                Block target = getNextLocation(search, loc);
-                if (target != null) {
-                    target = target.getRelative(0, -SEARCH_DEPTH, 0);
-                    Location newLoc = target.getLocation().add(0.5, 0.5, 0.5);
-                    loc = minecart.getLocation();
-                    Vector speed = newLoc.toVector().subtract(loc.toVector());
-                    Vector oldSpeed = minecart.getVelocity();
-                    oldSpeed.multiply(0.5);
-                    oldSpeed.setY(0);
-                    speed.add(oldSpeed);
-                    if (speed.lengthSquared() > 1) {
-                        speed.multiply(1.0 / (speed.length()));
-                    }
-                    minecart.setVelocity(speed);
-                }
+            Block search = getActualBlock(loc.getBlock(), loc);
+            if (search == null) {
+                return;
             }
+            loc = loc.add(minecart.getVelocity().multiply(4));
+            Block target = getNextLocation(search, loc);
+            if (target != null) {
+                target = target.getRelative(0, -SEARCH_DEPTH, 0);
+                Location newLoc = target.getLocation().add(0.5, 0.5, 0.5);
+                loc = minecart.getLocation();
+                Vector speed = newLoc.toVector().subtract(loc.toVector());
+                Vector oldSpeed = minecart.getVelocity();
+                oldSpeed.multiply(0.5);
+                oldSpeed.setY(0);
+                speed.add(oldSpeed);
+                if (speed.lengthSquared() > 1) {
+                    speed.multiply(1.0 / (speed.length()));
+                }
+                minecart.setVelocity(speed);
+            }
+
         }
     }
 
@@ -92,6 +105,32 @@ public class DiamondRails extends JavaPlugin implements Listener {
             }
         }
         return lastBlock;
+    }
+
+    private Block getActualBlock(Block scanBlock, Location calculatedHeading) {
+        double distance, lastDistance = Double.MAX_VALUE;
+        Block scannedBlock, lastBlock = null;
+        Location cache = new Location(calculatedHeading.getWorld(), 0, 0, 0);
+        for (BlockFace f : BLOCK_LOCATIONS) {
+            scannedBlock = f.getRelative(scanBlock);
+            if (scannedBlock.getType() == TRACK_MATERIAL) {
+                cache.setX(scannedBlock.getX() + 0.5);
+                cache.setY(scannedBlock.getY() + 0.5);
+                cache.setZ(scannedBlock.getZ() + 0.5);
+                distance = square(calculatedHeading.getX() - cache.getX())
+                        + square(calculatedHeading.getY() - cache.getY()) / 2
+                        + square(calculatedHeading.getZ() - cache.getZ());
+                if (distance < lastDistance) {
+                    lastDistance = distance;
+                    lastBlock = scannedBlock;
+                }
+            }
+        }
+        return lastBlock;
+    }
+
+    private static double square(double x) {
+        return x * x;
     }
 
     private static class BlockFace {
